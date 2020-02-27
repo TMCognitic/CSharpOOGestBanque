6fcf8a09140d3eb6b484593524d9663a6a79a6bf
TMCognitic <thierry.morre@cognitic.be>
2020-02-27 19:15:44 +0100

    Solution sur les propriétés

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 
 namespace GestBanque
 {
@@ -6,7 +7,35 @@ namespace GestBanque
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Personne chuckNorris = new Personne()
+            {
+                Nom = "Norris",
+                Prenom = "Chuck",
+                DateNaiss = new DateTime(1940, 3, 10)
+            };
+
+            Courant courant1 = new Courant()
+            {
+                Numero = "00001",
+                LigneDeCredit = 500,
+                Titulaire = chuckNorris
+            };
+
+            Courant courant2 = new Courant()
+            {
+                Numero = "00002",
+                Titulaire = chuckNorris
+            };
+
+            courant1.Depot(-500);
+            courant1.Depot(500);
+            courant1.Retrait(-750);
+            courant1.Retrait(750);
+
+            courant2.Depot(500);
+
+            Console.WriteLine($"Solde du compte '{courant1.Numero}' : {courant1.Solde}");
+            Console.WriteLine($"Solde du compte '{courant2.Numero}' : {courant2.Solde}");
         }
     }
 }
